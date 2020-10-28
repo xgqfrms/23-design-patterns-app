a1cc84f6af5f73755de42985fb9a3306f750065f
xgqfrms <xgqfrms@outlook.com>
2020-10-28 20:23:41 +0800

    Update main.dart

--- src/design_pattern_app/lib/main.dart
+++ src/design_pattern_app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '23 Design Pattern App',
       theme: ThemeData(
         // This is the theme of your application.
         //
